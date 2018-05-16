8fcc2ef79d57ac468d3a7be9ebab29213bdf296a
Arkadiusz Pituła <apitula@corelogic.pl>
2018-05-16 08:58:34 +0200

    Initial Commit

---
+++ Biometrics/Biometrics.h
@@ -0,0 +1,19 @@
+//
+//  Biometrics.h
+//  Biometrics
+//
+//  Created by Arkadiusz Pituła on 16.05.2018.
+//  Copyright © 2018 Arkadiusz Pituła. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Biometrics.
+FOUNDATION_EXPORT double BiometricsVersionNumber;
+
+//! Project version string for Biometrics.
+FOUNDATION_EXPORT const unsigned char BiometricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Biometrics/PublicHeader.h>
+
+
